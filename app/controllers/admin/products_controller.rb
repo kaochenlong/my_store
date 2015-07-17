1d4c7eb334624c8e8cbf641662aeea7d15a7dc8f
Eddie Kao <eddie@digik.com.tw>
2015-07-17 17:34:32 +0800

    CRUD for product backend

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -2,8 +2,13 @@ class Admin::ProductsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :admin_only!
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
 
   def index
+    @products = Product.all
+  end
+
+  def show
   end
 
   def new
@@ -15,13 +20,33 @@ class Admin::ProductsController < ApplicationController
     if @product.save
       redirect_to admin_products_path, notice:'成功新增商品'
     else
-      # ...
+      render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_products_path, notice:'成功更新商品'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to admin_products_path, notice:'商品已刪除'
   end
 
   private
   def product_params
     params.require(:product).permit(:name, :description, :price, :is_online, :image)
+  end
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
   end
 
   def admin_only!
