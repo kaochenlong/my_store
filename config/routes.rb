b3c0fa40a7d88d7f9fb87560dc33f999c67ae91b
Eddie Kao <eddie@digik.com.tw>
2015-07-17 17:36:55 +0800

    remove edit and delete function for product frontend

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root 'products#index'
 
-  resources :products
+  resources :products, only:[:index, :show]
 
   namespace :admin do
     resources :products
