652b6c53a1daee3466607b8f7dd66118fd5bfc43
Eddie Kao <eddie@digik.com.tw>
2015-07-18 16:01:27 +0800

    add cart to navbar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :initialize_cart
 
   def index
     @products = Product.online
