c7a3a412d030046dd267309ef141831f8ead95e6
Eddie Kao <eddie@digik.com.tw>
2015-08-01 14:33:27 +0800

    finish order button function

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,40 @@
 class Admin::OrdersController < Admin::BaseController
+  before_action :find_order, except: [:index]
 
   def index
     @orders = Order.all
+  end
+
+  def show
+  end
+
+  def pay
+    if @order.pay
+      redirect_to :back, notice: '狀態已變更'
+    else
+      redirect_to :back, notice: '狀態發生錯誤'
+    end
+  end
+
+  def out
+    if @order.out
+      redirect_to :back, notice: '狀態已變更'
+    else
+      redirect_to :back, notice: '狀態發生錯誤'
+    end
+  end
+
+  def deliver
+    if @order.deliver
+      redirect_to :back, notice: '狀態已變更'
+    else
+      redirect_to :back, notice: '狀態發生錯誤'
+    end
+  end
+
+  private
+  def find_order
+    @order = Order.find_by(id: params[:id])
   end
 
 end
